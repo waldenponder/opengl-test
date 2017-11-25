4114c4dea0cb1d59824453e97599a7ad528653c7
WaldenPonder <wangqianstu@163.com>
2017-11-26 00:34:02 +0800

    no message

--- a004/woodFrag.glsl
+++ a004/woodFrag.glsl
@@ -3,9 +3,10 @@
 out vec4 fragColor;
 
 in vec2 texCoor;
-in vec3 MCposition;
+in vec4 MCposition;
 
-/*	 Ä¾ÎÆ
+// Ä¾ÎÆ
+
 vec3 LightWood = vec3(0.827, 0.76, 0.4313);
 vec3 DarkWood = vec3(.7, .4, 0.3725);
 float RingFreq = 100;
@@ -13,14 +14,14 @@ float LightGrains = 1;
 float DarkGrains = 1;
 vec3 NoiseScale = vec3(1);
 float Noisiness = 10;
-float GrainScale = 0;
+float GrainScale = 0.1;
 float Lightness = .98;
 
 
 void main()
 {
-	vec3 noiseVec = noise3(MCposition * NoiseScale) * Noisiness;
-	vec3 location = MCposition + noiseVec;
+	vec3 noiseVec = noise3(MCposition.xyz * NoiseScale) * Noisiness;
+	vec3 location = MCposition.xyz + noiseVec;
 
 	float dis = sqrt(location.x * location.x + location.z * location.z);
 	dis *= RingFreq;
@@ -42,31 +43,32 @@ void main()
 	color *= Lightness;
 
 	fragColor = vec4(color, 1);
-}		  */
+}		  
+	 
 
 
-float LightIntensity = 1;
-
-const vec3 MarbleColor = vec3(0.7, 0.7, 0.7);
-const vec3 VeinColor = vec3(0.0, 0.15, 0.0);
-
-uniform sampler3D NoiseTex;
-uniform sampler1D SineTex;
-vec3 Offset = vec3(100);;
-
-void main(void)
-{
-	vec3 noisevec = noise3(MCposition + Offset.yzx).xyz; //texture3D(NoiseTex, MCposition + Offset.yzx);
-
-	float intensity = abs(noisevec[0] - 0.25) +
-		abs(noisevec[0] - 0.125) +
-		abs(noisevec[1] - 0.0625) +
-		abs(noisevec[2] - 0.03125);
-
-	//vec4 unswiz = texture1D(SineTex, MCposition.z + intensity * 2.0);
-	float sineval = sin(MCposition.z + intensity * 2.0);//.2;// unswiz.s;
-	vec3 color = mix(VeinColor, MarbleColor, sineval);
-	color *= sineval;
-	color = clamp(color, 0.0, 1.0);
-	fragColor = vec4(vec3(color), 1.0);
-}
+//float LightIntensity = 1;
+//
+//const vec3 MarbleColor = vec3(0.7, 0.7, 0.7);
+//const vec3 VeinColor = vec3(0.0, 0.15, 0.0);
+//
+//uniform sampler3D NoiseTex;
+//uniform sampler1D SineTex;
+//vec3 Offset = vec3(100);;
+//
+//void main(void)
+//{
+//	vec3 noisevec = noise3(MCposition); //texture3D(NoiseTex, MCposition + Offset.yzx);
+//
+//	float intensity =
+//		abs(noisevec[0] - 0.25) +
+//		abs(noisevec[1] - 0.125) +
+//		abs(noisevec[2] - 0.0625) +
+//		abs(1 - 0.03125);
+//
+//	//vec4 unswiz = texture1D(SineTex, MCposition.z + intensity * 2.0);
+//	float sineval = sin(MCposition.x * 6 + MCposition.y * 1 + intensity * 12.0) * .5 + .5;
+//	vec3 color = mix(VeinColor, MarbleColor, sineval);
+//	color = clamp(color, 0.0, 1.0);
+//	fragColor = vec4(vec3(color), 1.0);
+//}
--- a004/woodVert.glsl
+++ a004/woodVert.glsl
@@ -4,12 +4,12 @@ layout(location = 1) in vec2 coor;
 
 uniform mat4 vert_mat;
 out vec2 texCoor;
-out vec3 MCposition;
+out vec4 MCposition;
 
 void main()
 {
 	gl_Position = vert_mat * vec4(position.x, position.y, position.z, 1.0);
 	texCoor = coor;
 
-	MCposition = gl_Position.xyz;	
+	MCposition = gl_Position;	
 }
