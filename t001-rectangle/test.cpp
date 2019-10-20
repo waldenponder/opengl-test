f29d0877c0fc5f192e8759f21eb3640229e03bf7
wangstu <wangqianstu@163.com>
2019-10-20 15:25:55 +0800

    -

--- t001-rectangle/test.cpp
+++ t001-rectangle/test.cpp
@@ -41,7 +41,7 @@ const GLchar* frag = "#version 330 core\n"
 int main()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
